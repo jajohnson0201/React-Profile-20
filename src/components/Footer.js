e1c988aa16f52c184fd150870320248ceb0996aa
joe <jajohnson0201@gmail.com>
2023-03-29 19:37:06 -0500

    added content to the pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
-export default function Footer () {
-return (
-    <div>
-        <ul>
-            <li>
-                <a src="" href="" >
+export default function Footer() {
+  const [value, setValue] = React.useState(0);
 
-                </a>
-            </li>
-            <li>
-                <a src="" href="" >
-
-                </a>
-            </li>
-            <li>
-                <a src="" href="" >
-                    
-                </a>
-            </li>
-        </ul>
-    </div>
-);
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="jajohnson0201@gmail.com"  />
+        <BottomNavigationAction label="GitHub" href="https://github.com/jajohnson0201" />
+        <BottomNavigationAction label="LinkedIn" href="" />
+      </BottomNavigation>
+    </Box>
+  );
 }
